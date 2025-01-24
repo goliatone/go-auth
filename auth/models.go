e2709a1749ec5e15184615ede167198895737f35
goliatone <140582+goliatone@users.noreply.github.com>
2025-01-24 19:10:59 -0300

    add: base models for auth

---
+++ auth/models.go
@@ -0,0 +1,94 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/uptrace/bun"
+)
+
+// UserRole is the user's role
+type UserRole = string
+
+const (
+	// RoleGuest is an guest role
+	RoleGuest UserRole = "gest"
+	// RoleCustomer us a customer
+	RoleCustomer UserRole = "customer"
+	RoleViewer   UserRole = "viewer"
+	RoleEditor   UserRole = "editor"
+	// RoleAdmin is an admin role
+	RoleAdmin UserRole = "admin"
+)
+
+// User is the user model
+type User struct {
+	bun.BaseModel  `bun:"table:users,alias:usr"`
+	ID             uuid.UUID  `bun:"id,pk,nullzero,type:uuid" json:"id,omitempty"`
+	Role           UserRole   `bun:"user_role,notnull" json:"user_role,omitempty"`
+	FirstName      string     `bun:"first_name,notnull" json:"first_name,omitempty"`
+	LastName       string     `bun:"last_name,notnull" json:"last_name,omitempty"`
+	Username       string     `bun:"username,notnull,unique" json:"username,omitempty"`
+	Email          string     `bun:"email,notnull,unique" json:"email,omitempty"`
+	Phone          string     `bun:"phone_number" json:"phone_number,omitempty"`
+	PasswordHash   string     `bun:"password_hash" json:"password_hash,omitempty"`
+	EmailValidated bool       `bun:"is_email_verified" json:"is_email_verified,omitempty"`
+	LoginAttempts  int        `bun:"login_attempts" json:"login_attempts,omitempty"`
+	LoginAttemptAt *time.Time `bun:"login_attempt_at" json:"login_attempt_at,omitempty"`
+	LoggedInAt     *time.Time `bun:"loggedin_at" json:"loggedin_at,omitempty"`
+	ResetedAt      *time.Time `bun:"reseted_at,nullzero" json:"reseted_at,omitempty"`
+	CreatedAt      *time.Time `bun:"created_at,nullzero,default:current_timestamp" json:"created_at,omitempty"`
+	UpdatedAt      *time.Time `bun:"updated_at,nullzero,default:current_timestamp" json:"updated_at,omitempty"`
+	DeletedAt      *time.Time `bun:"deleted_at,soft_delete,nullzero" json:"deleted_at,omitempty"`
+}
+
+// PasswordResetStep step on password reset
+type PasswordResetStep = string
+
+const (
+	// ResetUnknown is the unknown status
+	ResetUnknown PasswordResetStep = "unknown"
+	// ResetInit is the initial step
+	ResetInit PasswordResetStep = "show-reset"
+	//AccountVerification notifiction sent
+	AccountVerification PasswordResetStep = "email-sent"
+	// ChangingPassword user will change password
+	ChangingPassword PasswordResetStep = "change-password"
+	// ChangeFinalized processing change
+	ChangeFinalized PasswordResetStep = "password-changed"
+)
+
+const (
+	// ResetUnknownStatus is the unknown status
+	ResetUnknownStatus = "unknown"
+	// ResetRequestedStatus is the requested status
+	ResetRequestedStatus = "requested"
+	// ResetExpiredStatus is the expired status
+	ResetExpiredStatus = "expired"
+	// ResetChangedStatus is the changed status
+	ResetChangedStatus = "changed"
+)
+
+// PasswordReset is the user model
+type PasswordReset struct {
+	bun.BaseModel `bun:"table:password_reset,alias:pwdr"`
+	ID            uuid.UUID  `bun:"id,pk,nullzero,type:uuid" json:"id,omitempty"`
+	UserID        *uuid.UUID `bun:"user_id,notnull" json:"user_id,omitempty"`
+	User          *User      `bun:"rel:has-one,join:user_id=id" json:"user,omitempty"`
+	Status        string     `bun:"status,notnull" json:"status,omitempty"`
+	Email         string     `bun:"email,notnull" json:"email,omitempty"`
+	DeletedAt     *time.Time `bun:"deleted_at,soft_delete,nullzero" json:"deleted_at,omitempty"`
+	ResetedAt     *time.Time `bun:"reseted_at,nullzero" json:"reseted_at,omitempty"`
+	CreatedAt     *time.Time `bun:"created_at,nullzero,default:current_timestamp" json:"created_at,omitempty"`
+	UpdatedAt     *time.Time `bun:"updated_at,nullzero,default:current_timestamp" json:"updated_at,omitempty"`
+}
+
+// MarkPasswordAsReseted will create a new instance
+func MarkPasswordAsReseted(id uuid.UUID) *PasswordReset {
+	r := &PasswordReset{}
+	r.ID = id
+	r.Status = ResetChangedStatus
+	n := time.Now()
+	r.ResetedAt = &n
+	return r
+}
