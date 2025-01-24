00f9cb03541ca99d080623137d036b28dfc892f4
goliatone <140582+goliatone@users.noreply.github.com>
2025-01-24 19:10:29 -0300

    add: password encryption

---
+++ auth/bcrypt.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// ErrMismatchedHashAndPassword failure to check password hash
+var ErrMismatchedHashAndPassword = errors.New("auth: hashedPassword is not the hash of the given password")
+
+// HashPassword will generate a password hash
+func HashPassword(password string) (string, error) {
+	h, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(h), err
+}
+
+// ComparePasswordAndHash will validate the given cleartext
+// password matches the hashed password
+func ComparePasswordAndHash(password, hash string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return ErrMismatchedHashAndPassword
+		}
+		return err
+	}
+	return nil
+}
+
+// RandomPasswordHash is a temporary password
+func RandomPasswordHash() string {
+	pwd := uuid.New()
+
+	h, err := HashPassword(pwd.String())
+	if err != nil {
+		return RandomPasswordHash()
+	}
+
+	return h
+}
