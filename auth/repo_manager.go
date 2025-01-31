0cd12d380830c6f81284fda3ccd1a26aa486154c
goliatone <140582+goliatone@users.noreply.github.com>
2025-01-31 18:47:59 -0300

    add: repo manager imp

--- auth/repo_manager.go
+++ auth/repo_manager.go
@@ -1,6 +1,11 @@
 package auth
 
 import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+
 	"github.com/goliatone/go-repository-bun"
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
@@ -37,4 +42,53 @@ func NewPasswordResetsRepository(db *bun.DB) repository.Repository[*PasswordRese
 
 func NewUsersRepository(db *bun.DB) Users {
 	return &users{db}
+}
+
+type mngr struct {
+	db             *bun.DB
+	users          Users
+	passwordResets repository.Repository[*PasswordReset]
+}
+
+func NewRepositoryManager(db *bun.DB) RepositoryManager {
+	return &mngr{
+		db:             db,
+		users:          NewUsersRepository(db),
+		passwordResets: NewPasswordResetsRepository(db),
+	}
+}
+
+func (m mngr) Validate() error {
+	if m.users == nil {
+		return errors.New("repository users should be initialized")
+	}
+
+	if m.passwordResets == nil {
+		return errors.New("repository passwordResets should be initialized")
+	}
+
+	return nil
+}
+
+func (m mngr) MustValidate() {
+	if err := m.Validate(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (m mngr) RunInTx(ctx context.Context, opts *sql.TxOptions, f func(ctx context.Context, tx bun.Tx) error) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		return m.db.RunInTx(ctx, opts, f)
+	}
+}
+
+func (m mngr) Users() Users {
+	return m.users
+}
+
+func (m mngr) PasswordResets() repository.Repository[*PasswordReset] {
+	return m.passwordResets
 }
