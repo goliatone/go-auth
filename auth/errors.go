8a1aa82c8e07217653097638c221da68d858561a
goliatone <140582+goliatone@users.noreply.github.com>
2025-02-03 15:35:20 -0300

    refactor: move related to errors

---
+++ auth/errors.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"errors"
+	"strings"
+)
+
+// ErrIdentityNotFound is the error we return for non found identities
+var ErrIdentityNotFound = errors.New("identity not found")
+
+// ErrUnableToFindSession is the error when our reequest has no cookie
+var ErrUnableToFindSession = errors.New("unable to find session")
+
+// ErrUnableToDecodeSession unable to decode JWT from session cookie
+var ErrUnableToDecodeSession = errors.New("unable to decode session")
+
+// ErrUnableToMapClaims unable to get claims from token
+var ErrUnableToMapClaims = errors.New("unable to map claims")
+
+// ErrUnableToParseData parse error
+var ErrUnableToParseData = errors.New("unable to parse data")
+
+// IsTokenExpiredError will check for expired tokens
+func IsTokenExpiredError(err error) bool {
+	return strings.Contains(err.Error(), "token is expired")
+}
+
+// IsMalformedError will check for error message
+func IsMalformedError(err error) bool {
+	return strings.Contains(err.Error(), "token is malformed") ||
+		strings.Contains(err.Error(), "missing or malformed JWT")
+}
