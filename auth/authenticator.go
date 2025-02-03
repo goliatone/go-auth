ad19c8dcd915ed2262784f192e414ad3677683cc
goliatone <140582+goliatone@users.noreply.github.com>
2025-02-03 15:35:13 -0300

    refactor: move related to errors

--- auth/authenticator.go
+++ auth/authenticator.go
@@ -5,26 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
-	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
-
-// ErrIdentityNotFound is the error we return for non found identities
-var ErrIdentityNotFound = errors.New("identity not found")
-
-// ErrUnableToFindSession is the error when our reequest has no cookie
-var ErrUnableToFindSession = errors.New("unable to find session")
-
-// ErrUnableToDecodeSession unable to decode JWT from session cookie
-var ErrUnableToDecodeSession = errors.New("unable to decode session")
-
-// ErrUnableToMapClaims unable to get claims from token
-var ErrUnableToMapClaims = errors.New("unable to map claims")
-
-// ErrUnableToParseData parse error
-var ErrUnableToParseData = errors.New("unable to parse data")
 
 type Auther struct {
 	provider        IdentityProvider
@@ -136,10 +120,4 @@ func (s Auther) generateJWT(identity Identity) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString([]byte(s.signingKey))
-}
-
-// IsMalformedError will check for error message
-func IsMalformedError(err error) bool {
-	return strings.Contains(err.Error(), "token is malformed") ||
-		strings.Contains(err.Error(), "missing or malformed JWT")
 }
