139ea378b1659ee02a8d21ccd2943f5e062f0b47
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-03 20:09:22 -0800

    refactor: migrations setup

--- data/sql/migrations/0001_auth0_identifiers.down.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS user_identifiers;
-
-DROP INDEX IF EXISTS users_external_id_unique;
-
-ALTER TABLE users
-    DROP COLUMN IF EXISTS external_id,
-    DROP COLUMN IF EXISTS external_id_provider;
